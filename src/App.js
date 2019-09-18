1f9bd1bbfccc5c12d02023eb466839b44ab382a8
Okera Johnson <okerajohnson@gmail.com>
2019-09-18 12:43:19 -0400

    Add files via upload
    Added dice that shake

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Rando from './Rando';
+import Button from './Button';
+import BrokenClick from './BrokenClick';
+import RollDice from './RollDice';
 import './App.css';
 
 class App extends Component {
     render() {
-        return <div className="App">
-            <Rando maxNum={7} />
+        return <div className='App'>
+            <RollDice />
         </div>;
     }
 }
